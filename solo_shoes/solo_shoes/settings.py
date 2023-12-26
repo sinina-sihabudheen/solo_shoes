71a1f4309809e6c205eca3301a574e94e5ec394d
sinina-sihabudheen <sinna.sihabudheen@gmail.com>
2023-12-26 21:00:23 +0530

    requrements.txt updated

--- solo_shoes/solo_shoes/settings.py
+++ solo_shoes/solo_shoes/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'solo_shoes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -100,6 +100,16 @@ DATABASES = {
 #         'PORT': '3306',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 
