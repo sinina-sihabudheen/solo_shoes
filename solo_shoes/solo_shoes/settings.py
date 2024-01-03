7a60ee8c051e29f00c79544211543c9da4a8464c
sinina-sihabudheen <sinna.sihabudheen@gmail.com>
2024-01-03 12:09:34 +0530

    new database created

--- solo_shoes/solo_shoes/settings.py
+++ solo_shoes/solo_shoes/settings.py
@@ -95,8 +95,8 @@ WSGI_APPLICATION = 'solo_shoes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
-        'USER': 'myprojectuser',
+        'NAME': 'myprojects',
+        'USER': 'myprojectusers',
         'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432',
