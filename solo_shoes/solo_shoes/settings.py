330f7af572d35a23a090331a02a24952c7d7a90c
sinina-sihabudheen <sinna.sihabudheen@gmail.com>
2024-02-26 10:55:36 +0530

    settings updated

--- solo_shoes/solo_shoes/settings.py
+++ solo_shoes/solo_shoes/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-gd__puppd$zxsy)27b2a$uc#%@ldjq2jc6lt^_1427ak373h1h'
+
 
 SECRET_KEY = config('SECRET_KEY')
 
@@ -100,8 +100,8 @@ DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'myprojects',
-#         'USER': 'myprojectusers',
-#         'PASSWORD': 'password',
+#         'USER': config('USER'),
+#         'PASSWORD': config('PASSWORD'),
 #         'HOST': 'localhost',
 #         'PORT': '5432',
 #     }
@@ -167,15 +167,8 @@ MEDIA_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Email configuration
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'  
-# EMAIL_PORT = 587  
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'sininasihabudheen@gmail.com'
-# EMAIL_HOST_PASSWORD = 'ugpd hzaw iuik howw'  
-# DEFAULT_FROM_EMAIL ='sininasihabudheen@gmail.com'
 
+# Email configuration
 EMAIL_BACKEND = config('EMAIL_BACKEND')
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
@@ -185,8 +178,8 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 
-LOGIN_URL = 'adminlogin'  # Assuming 'login' is the name of your login URL pattern
+LOGIN_URL = 'adminlogin'  
 
-# AUTH_USER_MODEL = 'user_authentication.User'
+
 
 
